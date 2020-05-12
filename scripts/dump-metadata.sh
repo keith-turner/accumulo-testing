bba22210c64a1140b70eb6a107170fa5dd35815d
Keith Turner <kturner@apache.org>
2020-05-12 18:48:06 -0400

    update dump script

--- scripts/dump-metadata.sh
+++ scripts/dump-metadata.sh
@@ -2,6 +2,6 @@
 
 while true
 do
-	accumulo shell -u root -p secret -e 'scan -t accumulo.metadata -b 2; -e 2< -c file -np' 2>/dev/null | grep file: | gzip > dump.$(date +%Y%m%d%H%M%S).gz
+	accumulo shell -u root -p secret -e 'scan -t accumulo.metadata -b 2; -e 2< -c file -np' 2>/dev/null | grep file: | gzip > dumps/dump.$(date +%Y%m%d%H%M%S).gz
 	sleep 30
 done
