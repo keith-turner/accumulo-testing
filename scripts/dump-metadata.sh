1d02cdaf77e0f80073f61bf89b2a4004d74feaf9
Keith Turner <kturner@apache.org>
2020-05-12 15:44:32 -0400

    changes for compaction test

---
+++ scripts/dump-metadata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+	accumulo shell -u root -p secret -e 'scan -t accumulo.metadata -b 2; -e 2< -c file -np' 2>/dev/null | grep file: > dump.$(date +%Y%m%d%H%M)
+	sleep 30
+done
