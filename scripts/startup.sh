ef208b30c17c0d27562d16122ac29e2f9fb1363d
Keith Turner <kturner@apache.org>
2020-05-26 19:46:13 -0400

    Update test config

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,6 +5,14 @@ docker build --build-arg HADOOP_HOME=$HADOOP_HOME --build-arg HADOOP_USER_NAME=`
 #TODO set compaction threads
 accumulo shell -u root -p secret -e "config -s tserver.compaction.major.concurrent.max=6"
 
+
+config -s 'tserver.compaction.service.user.planner=org.apache.accumulo.core.spi.compaction.DefaultCompactionPlanner'
+config -s 'tserver.compaction.service.user.planner.opts.executors=[{"name":"all","numThreads":2}]'
+config -s 'tserver.compaction.service.user.planner.opts.maxOpen=30'
+config -s 'tserver.compaction.service.default.planner=org.apache.accumulo.core.spi.compaction.DefaultCompactionPlanner'
+config -s 'tserver.compaction.service.default.planner.opts.executors=[{"name":"small","maxSize":"32M","numThreads":2},{"name":"large","numThreads":2}]'
+config -s 'tserver.compaction.service.default.planner.opts.maxOpen=10'
+
 while read host; do
   docker save accumulo-testing | ssh -C $host docker load &
 done < $ACCUMULO_HOME/conf/tservers
